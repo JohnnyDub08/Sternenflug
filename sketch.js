ec3bd60645572aed9556f2fea48481bf53de9c0d
John Dup <34498467+JohnnyDub08@users.noreply.github.com>
2026-03-30 11:20:58 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,34 @@
 let sterne = [];
-
 let speed;
 
 function setup() {
-  //setAttributes('antialias', true);
-  canvas = createCanvas(windowWidth, windowHeight, WEBGL)
+  // WEBGL für 3D-Effekt nutzen
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.position(0, 0);
-  canvas.style('z-index', '-1')
-  canvas.elt.style.position = "fixed"
-  for (let i = 0; i < windowWidth+windowHeight; i++) {
+  canvas.style('z-index', '-1');
+  canvas.elt.style.position = "fixed";
+
+  // Initialisierung der Sterne
+  let sternAnzahl = windowWidth + windowHeight;
+  for (let i = 0; i < sternAnzahl; i++) {
     sterne[i] = new Stern();
   }
-  speed = (windowWidth+windowHeight)/270;
+  
+  calculateSpeed();
 }
 
 function windowResized() {
-  canvas = createCanvas(windowWidth, windowHeight, WEBGL)
-  canvas.position(0, 0);
-  canvas.style('z-index', '-1')
-  canvas.elt.style.position = "fixed"
-  for (let i = 0; i < windowWidth+windowHeight; i++) {
-    sterne[i] = new Stern();
-  }
-  speed = (windowWidth+windowHeight)/270;
+  resizeCanvas(windowWidth, windowHeight);
+  calculateSpeed();
+}
+
+function calculateSpeed() {
+  speed = (windowWidth + windowHeight) / 270;
 }
 
 function draw() {
-  //speed = 12;
   background(0);
+  
   for (let i = 0; i < sterne.length; i++) {
     sterne[i].update();
     sterne[i].show();
@@ -35,26 +36,44 @@ function draw() {
 }
 
 function Stern() {
+  // Zufällige Position im 3D-Raum
   this.x = random(-width, width);
   this.y = random(-height, height);
   this.z = random(width);
+  this.pz = this.z; // Vorheriges Z für Motion-Blur-Effekte (optional)
 
   this.update = function() {
     this.z = this.z - speed;
+    
+    // Wenn der Stern am Betrachter vorbei ist, zurücksetzen
     if (this.z < 1) {
       this.z = width;
       this.x = random(-width, width);
       this.y = random(-height, height);
+      this.pz = this.z;
     }
   };
 
   this.show = function() {
-    var dist = map(this.z, width , 0 , 0 , 400)
-    stroke(dist);
-    strokeWeight(3);
-    var sx = map(this.x / this.z, 0, 1, 0, width);
-    var sy = map(this.y / this.z, 0, 1, 0, height);
-    var r = map(this.z, 0, width, 9, 0);
-    point(sx, sy, r);
+    fill(255);
+    noStroke();
+
+    // Projektion von 3D auf 2D
+    // sx = x / z ergibt den perspektivischen Effekt
+    let sx = map(this.x / this.z, 0, 1, 0, width);
+    let sy = map(this.y / this.z, 0, 1, 0, height);
+
+    // Größe des Sterns basierend auf der Nähe (Z-Wert)
+    let r = map(this.z, 0, width, 8, 0);
+    
+    // Leuchteffekt / Helligkeit basierend auf Nähe
+    let helligkeit = map(this.z, 0, width, 255, 50);
+    stroke(helligkeit);
+    strokeWeight(r);
+    
+    // Punkt zeichnen (in WEBGL ist 0,0 die Mitte)
+    point(sx, sy);
+    
+    this.pz = this.z;
   };
 }
